a279a5e14f36609c1b590ae96d7960e3a4307f78
choiyoubin <48716638+choiyoubin@users.noreply.github.com>
2024-01-08 23:00:45 +0900

    intent 오류 수정

--- components/WebviewContainer.js
+++ components/WebviewContainer.js
@@ -1,20 +1,42 @@
+import { Linking, Platform } from "react-native";
+import SendIntentAndroid from 'react-native-send-intent';
 import { WebView } from "react-native-webview";
 
 const WebviewContainer = ({ handleSetRef, handleEndLoading }) => {
   const uri = "https://test.readyvery.com";
 
-  /** 웹뷰에서 rn으로 값을 보낼때 거치는 함수 */
-  const handleOnMessage = ({ nativeEvent: { data } }) => {
-    // data에 웹뷰에서 보낸 값이 들어옵니다.
-    console.log(data);
-  };
+  const onShouldStartLoadWithRequest = (event) => {
+    // URL
+    if (event.url.startsWith("http://") || event.url.startsWith("https://") || event.url.startsWith("about:blank")){
+        return true;
+      }
+          // android
+          if (Platform.OS === "android") {
+            // Android: SendIntentAndroid 사용하여 앱 열기
+            SendIntentAndroid.openAppWithUri(event.url)
+              .then((isOpened) => {
+                if (!isOpened) {
+                  alert("앱을 열 수 없습니다.");
+                }
+              }).catch((err) => {
+                console.log(err);
+              });
+          } else if (Platform.OS === "ios") {
+            // iOS: Linking 사용하여 앱 열기
+            Linking.openURL(event.url).catch((err) => {
+              alert("앱을 열 수 없습니다.");
+            });
+          }
+          return false; 
+        }
 
   return (
     <WebView
       onLoadEnd={handleEndLoading}
-      onMessage={handleOnMessage}
+      onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
       ref={handleSetRef}
       source={{ uri }}
+      javaScriptEnabled={true}
     />
   );
 };
