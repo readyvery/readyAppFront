6eb85fba2c846f31a922dd5bb9327a7a7d3dc38e
choiyoubin <48716638+choiyoubin@users.noreply.github.com>
2024-01-11 17:19:52 +0900

    FCM 테스트 코드 추가

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
+import messaging from '@react-native-firebase/messaging';
+import { useEffect } from 'react';
 import WebviewContainer from './components/WebviewContainer';
 
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('[Background Remote Message]', remoteMessage);
+});
+
 const App = () => {
-   
+  
+  const getFcmToken = async () => {
+    const fcmToken = await messaging().getToken();
+    console.log('[FCM Token] ', fcmToken);
+  }
+
+  useEffect(() => {
+    getFcmToken();
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('[Remote Message] ', JSON.stringify(remoteMessage));
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <WebviewContainer/> );
 };
+
 export default App;
