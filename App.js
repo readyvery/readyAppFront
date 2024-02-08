8917309910d176205b288224b13af43928684518
choiyoubin <48716638+choiyoubin@users.noreply.github.com>
2024-02-08 14:42:03 +0900

    화면 규격 설정

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import messaging from '@react-native-firebase/messaging';
 import { useEffect } from 'react';
+//import { Platform, StyleSheet, View } from 'react-native';
 import WebviewContainer from './components/WebviewContainer';
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
@@ -22,7 +23,25 @@ const App = () => {
   }, []);
 
   return (
-    <WebviewContainer/> );
+    //<SafeAreaView style={styles.container}>
+   // <View style={styles.bottomSafeArea}>
+      <WebviewContainer/> 
+    //</View>
+   //</SafeAreaView>
+    );
 };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//   },
+// })
+
+// const styles = StyleSheet.create({
+//   bottomSafeArea: {
+//     flex: 1,
+//     paddingBottom: Platform.OS === 'ios' ? 34 : 0, // iPhone X 이상 모델의 하단 Safe Area 높이를 고려한 값
+//   },
+// });
 
 export default App;
